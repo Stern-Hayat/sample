8ffac43b32b11c297ae71724a7447b7e4b3dd9ea
堀内　竣平 <horiuchishunhei@horiuchishunheinoMacBook.local>
2019-05-29 18:54:32 +0900

    vue.jsde

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <form action="">
+      <div>
+        名前 <input type="text" name="name" v-model="form.name">
+        お問いあわせないよう <input type="text" name="name" v-model="form.name">
+        <div v-if="error.name">名前を入力して下さい。</div>
+      </div>
+      <input type="submit" value="送信" @click="submit">
+
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        form: {
+          name: ""
+        },
+        error: {
+          name: false
+        }
+      }
+    },
+    methods: {
+      submit(e) {
+        this.error.name = true
+        e.preventDefault()
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
